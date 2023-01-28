0e7ad20c0a90260b6682371b524b866ed3209c5a
Minh Tri <daominhtri1000@gmail.com>
2023-01-28 16:31:43 +0700

    add something

---
+++ Spring demo/hibernate-tutorial/src/main/java/com/spring/hibernate/tutorial/CreateStudentDemo.java
@@ -0,0 +1,40 @@
+package com.spring.hibernate.tutorial;
+
+import com.spring.hibernate.tutorial.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateStudentDemo {
+
+    public static void main(String[] args) {
+
+        //Create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        //Create session
+        Session session = factory.getCurrentSession();
+        
+        try {
+            //Create student object
+            System.out.println("Creating the student object....");
+            Student tmpStudent = new Student("Dao", "Minh Tri", "abc@gmail.com");
+            
+            //Start transaction
+            System.out.println("Starting the transaction....");
+            session.beginTransaction();
+            
+            //Svae student object
+            System.out.println("Saving the transaction");
+            session.save(tmpStudent);
+            
+            //Commit the transaction
+            session.getTransaction().commit(); 
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
